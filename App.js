39b2fe9d855dae4387d91dd60a1367b49c1795a2
shelune <hieu@mios.fi>
2017-12-22 13:39:25 +0200

    add devtool debugger

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 import reducers from './src/reducers';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'remote-redux-devtools';
 import ReduxThunk from 'redux-thunk';
 
 import Router from './src/Router';
 
 import Header from './src/components/Header';
 
-const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+const composeEnhancers = composeWithDevTools({ 
+  realtime: true, 
+  port: 5678, 
+  name: Platform.OS, 
+  hostname: 'localhost' 
+});
+const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(ReduxThunk)));
 // console.log(store.getState());
 
 export default class App extends React.Component {
